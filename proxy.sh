630435f28caea035c61af81c984a1e0c05db1417
Ken.L.Ma <ken.l.ma@newegg.com>
2017-08-29 14:22:48 -0700

    proxy file

---
+++ proxy.sh
@@ -0,0 +1,11 @@
+MY_PROXY_URL='http://s1firewall:8080/'
+HTTP_PROXY=$MY_PROXY_URL
+HTTPS_PROXY=$MY_PROXY_URL
+FTP_PROXY=$MY_PROXY_URL
+http_proxy=$MY_PROXY_URL
+https_proxy=$MY_PROXY_URL
+ftp_proxy=$MY_PROXY_URL
+no_proxy='localhost,127.0.0.1'
+NO_PROXY=$no_proxy
+
+export HTTP_PROXY HTTPS_PROXY FTP_PROXY http_proxy https_proxy ftp_proxy no_proxy NO_PROXY
